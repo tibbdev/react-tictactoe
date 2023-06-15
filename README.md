d42442bcab523de71615808357d6c9734ecc4d1f
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-06-15 10:09:40 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-tictactoe
+
+based on this video : https://www.youtube.com/watch?v=MRe-5nVBPRo&list=TLPQMTQwNjIwMjO3Ioz-0aRIYg&index=2
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,40 +1,107 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Square from "./Square"
 
-const INITIAL_GAME_STATE = ["","","",
-                            "","","",
-                            "","",""]
+const INITIAL_GAME_STATE = [
+         "","","",
+         "","","",
+         "","",""
+      ]
+
+const WINNING_COMBOS = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+]
+
 function Game() {
-  const [gameState, setGameState] = useState<string[]>(INITIAL_GAME_STATE)
+   const [gameState, setGameState] = useState<string[]>(INITIAL_GAME_STATE)
+   const [currentPlayer, setCurrentPlayer] = useState<string>("X")
 
-  const handleCellClick = (event: any) => {
-    console.log("Cell Clicked!", event.target.getAttribute("data-cell-index"));
-};
+   useEffect(() => {
+      changePlayer();
+   }, [gameState])
 
-  return (
+   const changePlayer = () => {
+      checkForWinner()
+   }  
+   const checkForWinner = () => {
+      let roundWon = false
+
+      for (let index = 0; index < WINNING_COMBOS.length; index++) {
+         const winCombo = WINNING_COMBOS[index];
+         
+         let a = gameState[winCombo[0]];
+         let b = gameState[winCombo[1]];
+         let c = gameState[winCombo[2]];
+
+         if([a,b,c].includes("")) {
+            continue;
+         }
+
+         if((a === b) && (b === c))
+         {
+            roundWon = true;
+            break;
+         }
+      }
+
+      if(roundWon) {
+         window.alert(`Congrats Player ${currentPlayer}! You are the winner!!`)
+         return;
+      }
+
+      if (!gameState.includes[""]) {
+         window.alert(`The game ended in a draw. You're both losers!`)
+         return;
+      }
+
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+   }
+
+   const handleCellClick = (event: any) => {
+      const cellIndex = Number(event.target.getAttribute("data-cell-index"))
+      
+      const currentValue = gameState[cellIndex];
+      console.log(" ~ FILE: Game.tsx ~ line.16 ~ handleCellClick ~ currentValue", currentValue);
+
+      if (currentValue) { // Do not modify cell if there is already a value stored there
+         return
+      }
+
+      const newValues = [...gameState]
+      newValues[cellIndex] = currentPlayer
+      setGameState(newValues)
+   };
+
+   return (
       <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
-        <h1 className="text-center text-7xl mb-4 font-display text-white">
-          Tic Tac Toe (React Edition) - Game Page
-        </h1>
-        <div>
-          <div className="grid grid-cols-3 gap-3 mx-auto w-96">
-            {
-              gameState.map((player, index) =>
-                <Square 
-                  key={index} 
-                  onClick={handleCellClick}
-                    {...{index, player}}
-                  />
-              )
-            }
-          </div>
-          <div>
-            Score Goes Here...
-          </div>
-        </div>
+         <h1 className="text-center text-7xl mb-4 font-display text-white">
+            Tic Tac Toe (React Edition) - Game Page
+         </h1>
+         <div>
+            <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+               {
+               gameState.map(
+                  (player, index) =>
+                  <Square 
+                     key={index} 
+                     onClick={handleCellClick}
+                     {...{index, player}}
+                     />
+               )
+               }
+            </div>
+            <div>
+               Score Goes Here...
+            </div>
+         </div>
       </div>
-
-  )
+   )
 }
 
 export default Game
